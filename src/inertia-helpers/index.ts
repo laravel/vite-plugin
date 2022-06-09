a7a97c1848c61d344f00d647381395467b0835ec
Esi're <esiri92@gmail.com>
2022-06-09 19:56:47 +0100

    parenthesize function type.

--- src/inertia-helpers/index.ts
+++ src/inertia-helpers/index.ts
@@ -1,4 +1,4 @@
-export async function resolvePageComponent<T>(path: string, pages: Record<string, T | () => Promise<T>>) {
+export async function resolvePageComponent<T>(path: string, pages: Record<string, T | (() => Promise<T>)>) {
     const page = pages[path]
     if (typeof page === 'undefined') {
         throw new Error(`Page not found: ${path}`)
