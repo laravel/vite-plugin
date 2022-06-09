58c8b3c83118523c70e7102501b8b8396389df97
Esirei <esiri92@gmail.com>
2022-06-09 19:33:27 +0100

    Update resolvePageComponent helper to work with both glob and glogEager
    This pull request updates the `resolvePageComponent` inertia helper to work with the object returned both by the glob and globEager imports.

--- src/inertia-helpers/index.ts
+++ src/inertia-helpers/index.ts
@@ -1,7 +1,8 @@
-export function resolvePageComponent<T>(path: string, pages: Record<string, () => Promise<T>>) {
-    if (typeof pages[path] === 'undefined') {
+export async function resolvePageComponent<T>(path: string, pages: Record<string, T | () => Promise<T>>) {
+    const page = pages[path]
+    if (typeof page === 'undefined') {
         throw new Error(`Page not found: ${path}`)
     }
 
-    return pages[path]()
+    return typeof page === 'function' ? await page() : page
 }
