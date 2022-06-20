9703acafa72faeae26075a189f971f48ec06c889
Tim MacDonald <hello@timacdonald.me>
2022-06-20 10:45:57 +1000

    formatting

--- src/inertia-helpers/index.ts
+++ src/inertia-helpers/index.ts
@@ -1,8 +1,9 @@
-export async function resolvePageComponent<T>(path: string, pages: Record<string, T | (() => Promise<T>)>) {
+export async function resolvePageComponent<T>(path: string, pages: Record<string, Promise<T> | (() => Promise<T>)>): Promise<T> {
     const page = pages[path]
+
     if (typeof page === 'undefined') {
         throw new Error(`Page not found: ${path}`)
     }
 
-    return typeof page === 'function' ? await (page as () => Promise<T>)() : page
+    return typeof page === 'function' ? page() : page
 }
