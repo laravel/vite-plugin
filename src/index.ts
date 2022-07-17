0d4429aa5b154ec6de2afd28bc276a55fcc432cf
Tim MacDonald <hello@timacdonald.me>
2022-07-17 13:34:54 +1000

    remove workaround

--- src/index.ts
+++ src/index.ts
@@ -348,10 +348,7 @@ function noExternalInertiaHelpers(config: UserConfig): true|Array<string|RegExp>
     /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
     /* @ts-ignore */
     const userNoExternal = (config.ssr as SSROptions|undefined)?.noExternal
-    const pluginNoExternal = [
-        'laravel-vite-plugin/inertia-helpers',
-        '@inertiajs/server'
-    ]
+    const pluginNoExternal = ['laravel-vite-plugin/inertia-helpers']
 
     if (userNoExternal === true) {
         return true
