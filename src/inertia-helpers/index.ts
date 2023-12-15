4cdc8ff9e4e2720997cc3a4d78f29503b74fcee3
Tim MacDonald <hello@timacdonald.me>
2023-12-15 16:01:18 +1100

    Allow path to be a list

--- src/inertia-helpers/index.ts
+++ src/inertia-helpers/index.ts
@@ -1,9 +1,13 @@
-export async function resolvePageComponent<T>(path: string, pages: Record<string, Promise<T> | (() => Promise<T>)>): Promise<T> {
-    const page = pages[path]
+export async function resolvePageComponent<T>(path: string|string[], pages: Record<string, Promise<T> | (() => Promise<T>)>): Promise<T> {
+    for (const p of (Array.isArray(path) ? path : [path])) {
+        const page = pages[p]
 
-    if (typeof page === 'undefined') {
-        throw new Error(`Page not found: ${path}`)
+        if (typeof page === 'undefined') {
+            continue
+        }
+
+        return typeof page === 'function' ? page() : page
     }
 
-    return typeof page === 'function' ? page() : page
+    throw new Error(`Page not found: ${path}`)
 }
