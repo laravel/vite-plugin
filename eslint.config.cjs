1fe90f81685362644210fb5a501edb783b6f0122
Tim MacDonald <hello@timacdonald.me>
2025-07-08 10:20:14 +1000

    Upgrade dependencies

---
+++ eslint.config.cjs
@@ -0,0 +1,38 @@
+const {
+    defineConfig,
+} = require("eslint/config");
+
+const globals = require("globals");
+const tsParser = require("@typescript-eslint/parser");
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = defineConfig([{
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+
+        parser: tsParser,
+    },
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+
+    rules: {
+        "no-trailing-spaces": "error",
+    },
+}]);
